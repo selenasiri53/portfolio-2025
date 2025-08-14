2bdfb71347eb80bb2b0b78050036f66d27295f41
Selena Siri <selenasiri53@gmail.com>
2025-08-14 11:52:52 -0400

    rm useOutsideClick comment

--- app/aceternity/ui/apple-cards-carousel.tsx
+++ app/aceternity/ui/apple-cards-carousel.tsx
@@ -15,7 +15,6 @@ import {
 import { cn } from "@/lib/utils";
 import { AnimatePresence, motion } from "motion/react";
 import Image, { ImageProps } from "next/image";
-// import { useOutsideClick } from "../hooks/use-outside-click";
 
 interface CarouselProps {
   items: JSX.Element[];
@@ -183,8 +182,6 @@ export const Card = ({
     window.addEventListener("keydown", onKeyDown);
     return () => window.removeEventListener("keydown", onKeyDown);
   }, [open]);
-
-  // useOutsideClick(containerRef, () => handleClose());
 
   const handleOpen = () => {
     if (!card.link) {
