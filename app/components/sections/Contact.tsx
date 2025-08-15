b52973c5f82d710936e0cc3d698935b2148fff2a
Selena Siri <selenasiri53@gmail.com>
2025-08-15 19:06:23 -0400

    contact

--- app/components/sections/Contact.tsx
+++ app/components/sections/Contact.tsx
@@ -6,13 +6,13 @@ import { FaLinkedin, FaGithub, FaGlobe } from "react-icons/fa";
 export default function ContactPage() {
   return (
     <div className="mx-4 md:mx-auto lg:mx-32 md:max-w-7xl h-auto py-16 flex flex-col justify-center items-center max-w-7xl px-4 bg-indigo-50 rounded-4xl my-20">
-      <h2 className="text-3xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans mb-8">
+      <h2 className="text-3xl md:text-5xl font-bold text-neutral-600 dark:text-neutral-200 font-sans mb-8">
         Contact
       </h2>
 
-      <div className="flex flex-col space-y-6 text-lg md:text-xl text-neutral-800 items-center text-center">
+      <div className="flex flex-col space-y-6 text-lg md:text-xl text-neutral-600 items-center text-center">
         <div>
-          <span>Email: </span>
+          <span className="text-rose-300">Have a project in mind?</span>
           <a
             href="mailto:selenasiri53@gmail.com"
             className="ml-2 underline hover:text-blue-300 transition-colors"
